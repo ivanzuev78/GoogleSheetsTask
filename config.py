85e7ccf977c6d0f47cd151e6f68bb5bf823f2d7e
Иван Зуев <ivan78.ru@yandex.ru>
2022-09-29 21:49:33 +0300

    Добавил файл логов в конфиг

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ GOOGLE_SHEET_NAME = config["DEFAULT"]["google_sheet_name"]
 PATH_TO_CREDENTIALS = config["DEFAULT"]["path_to_credentials"]
 CBR_URL = config["DEFAULT"]["cbr_url"]
 log_level = int(config["DEFAULT"]["log_level"])
+log_file = config["DEFAULT"]["log_file"]
 
 refresh_time_sec = int(config["DEFAULT"]["refresh_time_sec"])
 exchange_rate_update_sec = int(config["DEFAULT"]["exchange_rate_update_sec"])
@@ -17,4 +18,4 @@ order_numb_col = int(config["COL_MAP"]["order_numb"])
 cost_usd_col = int(config["COL_MAP"]["cost_usd"])
 delivery_date_col = int(config["COL_MAP"]["delivery_date"])
 
-logging.basicConfig(filename="example.log", filemode="w", level=log_level)
+logging.basicConfig(filename=log_file, level=log_level)
